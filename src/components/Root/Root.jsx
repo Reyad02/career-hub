1678ccd3f842db9396427c39dc6ab837afaaece6
unknown <areyad203011@bscse.uiu.ac.bd>
2024-03-23 16:49:13 +0600

    project initiated

---
+++ src/components/Root/Root.jsx
@@ -0,0 +1,9 @@
+const Root = () => {
+    return (
+        <div>
+            <h2 className='text-2xl font-semibold'>This is the root component!</h2>
+        </div>
+    );
+};
+
+export default Root;
